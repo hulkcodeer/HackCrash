3fdf3b871fc9163c8e9601e584e86946f14ed0a2
hyunjin park <hulkcodeer@gmail.com>
2024-06-01 23:27:47 +0900

    :construction: Add Player

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    private float moveSpeed = 3f;
+    private readonly float moveSpeed = 3f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.down * moveSpeed;
+        transform.position += moveSpeed * Time.deltaTime * Vector3.down;
+        if (transform.position.y < -10)
+        {
+            transform.position += new Vector3(0, 20f, 0);
+        }
     }
 }
