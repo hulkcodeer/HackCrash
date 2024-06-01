e10c768a29b291ba721a8e08b1ea81234db50506
hyunjin park <hulkcodeer@gmail.com>
2024-06-01 23:10:52 +0900

    Initial commit

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private float moveSpeed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.down * moveSpeed;
+    }
+}
