742a8699d14637b85d14aeab32107b0b8d5df30c
hyunjin park <hulkcodeer@gmail.com>
2024-06-03 14:37:38 +0900

    :construction: Add Charcter Move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        // float horizontalInput = Input.GetAxisRaw("Horizontal");
+        // // float verticalInput = Input.GetAxisRaw("Vertical");
+        // Vector3 moveTo = new(horizontalInput, 0f, 0f);
+        // transform.position += moveSpeed * Time.deltaTime * moveTo;
+
+        //  키보드 제어
+        // Vector3 moveTo = new(moveSpeed * Time.deltaTime, 0, 0);
+        // if (Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     transform.position -= moveTo;
+        // }
+        // else if (Input.GetKey(KeyCode.RightArrow))
+        // {
+        //     transform.position += moveTo;
+        // }
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float toX = Math.Clamp(mousePos.x, -2.35f, 2.35f);
+        transform.position = new Vector3(toX, transform.position.y, 0f);
+    }
+}
